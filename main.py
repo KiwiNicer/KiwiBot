087d07746bc7b6ee158a6985ab09f843fd76b254
CakesTwix <oleg.kiryazov@gmail.com>
2020-10-25 13:25:17 +0200

    main: Added check 18+ tags

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import telebot
 import requests
 import xml.etree.cElementTree as ET
 
-bot = telebot.TeleBot('1012306043:AAG9Gr-QUXn4xX5iKIg5xAKl-M9kfTyNeKg')
+bot = telebot.TeleBot('Токен сюда')
+
+tags=["no_bra", "tits", "boobs", "anus", "pussy", "uncensored", "censored", "open shirt", "trap", "yaoi", "cum", "penis", "milk", "pantsu","sex"] 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -12,10 +14,18 @@ def start_message(message):
 def photo_message(message):
 	response = requests.get('https://yande.re/post.json?limit=1')
 	Json = response.json()
-	print(Json[0]["sample_url"])
-	try:
-		bot.send_photo(message.chat.id, Json[0]["sample_url"])    
-	except:		
-		bot.send_message(message.chat.id, "Чет тг не понрав")
+	print(Json[0]["tags"])
+	for tagsx in tags:
+		if (tagsx in Json[0]["tags"].split()):
+			bot.send_message(message.chat.id,"Ага, 18+")
+			break
+		else:
+			#print(Json)
+			try:
+				#print(tags in Json[0]["tags".split()])
+				bot.send_photo(message.chat.id, Json[0]["sample_url"])    
+			except:
+				bot.send_message(message.chat.id, "Чет тг не понрав")
+		break
 bot.polling()
 
